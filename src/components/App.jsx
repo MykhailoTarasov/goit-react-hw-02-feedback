012910621690877b820fc7ec4609955278b1e024
MykhailoTarasov <123101959+MykhailoTarasov@users.noreply.github.com>
2023-12-14 14:26:32 +0200

    done with next task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,78 +1,71 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import Statistics from './Statistics/Statistics';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
 
-
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+const App = () => {
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
 
   // Made one function for all three buttons
-  handlerButtonTags = option => {
-    this.setState(prevState => {
+  const handlerButtonTags = (option) => {
+    setFeedback(prevState => {
       return {
+        ...prevState,
         [option]: prevState[option] + 1,
       };
     });
   };
 
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((acc, value) => acc + value);
+  const countTotalFeedback = () => {
+    return Object.values(feedback).reduce((acc, value) => acc + value);
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
+  const countPositiveFeedbackPercentage = () => {
+    const { good } = feedback;
+    const total = countTotalFeedback();
     const percentage = total > 0 ? (good / total) * 100 : 0;
 
     return percentage.toFixed(0);
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const tags = Object.keys(this.state);
+  const tags = Object.keys(feedback);
+  const { good, neutral, bad } = feedback;
 
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          fontSize: 20,
-          color: '#010101',
-          backgroundColor: '#c1eab2'
-        }}
-      >
-        <div>
-          <Section title="Please leave feedback">
-            <FeedbackOptions
-              options={tags}
-              onLeaveFeedback={this.handlerButtonTags}
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        fontSize: 20,
+        color: '#010101',
+        backgroundColor: '#c1eab2',
+      }}
+    >
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={tags}
+            onLeaveFeedback={handlerButtonTags}
+          />
+
+          {countTotalFeedback() ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={countTotalFeedback()}
+              positivePercentage={countPositiveFeedbackPercentage()}
             />
-
-            {/* Adding Notification message */}
-            {this.countTotalFeedback() ? (
-              <Statistics
-                good={good}
-                neutral={neutral}
-                bad={bad}
-                total={this.countTotalFeedback()}
-                positivePercentage={this.countPositiveFeedbackPercentage()}
-              />
-            ) : (
-              <Notification message="There is no feedback" />
-            )}
-          </Section>
-        </div>
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
